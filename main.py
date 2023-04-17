1a0bc9e1eb24684dbea8000903a267a4df94ceb7
Deyner Ribon <deynerdev@gmail.com>
2023-04-17 15:40:33 -0500

    code refactoring

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from random import *
-import string
+from string import ascii_lowercase, ascii_uppercase, punctuation
 
 def generate_password(length):
     password_length = length
-    lower_case = string.ascii_lowercase
-    upper_case = string.ascii_uppercase
-    special_characters = string.punctuation
+    lower_case = ascii_lowercase
+    upper_case = ascii_uppercase
+    special_characters = punctuation
     source = lower_case + upper_case + special_characters
     password = sample(source, password_length) 
     password = "".join(password)
     return password
 
+print(generate_password(8))
